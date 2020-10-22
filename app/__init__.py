580a72a479ddefb55907ebc261a73452ceeeb237
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-10-22 16:41:37 +0300

    Bootstrap basic flask app

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+"""Bootstrap the app"""
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+app.config.from_object("config")
+
+
+# Sample HTTP error handling
+@app.errorhandler(404)
+def not_found(error):
+    """Display this for any path that returns a 404"""
+    return render_template('404.html', error=error), 404
