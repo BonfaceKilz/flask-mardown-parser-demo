c6bd0275eddae2f1911a94e2e91d0c0d9768fcbf
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-10-25 13:49:02 +0300

    Add basic index ("/") route

--- app/__init__.py
+++ app/__init__.py
@@ -12,3 +12,10 @@ app.config.from_object("config")
 def not_found(error):
     """Display this for any path that returns a 404"""
     return render_template('404.html', error=error), 404
+
+
+@app.route("/", methods=["get"])
+def index():
+    """Display homepage which is normal html"""
+    return render_template("index.html"), 200
+
