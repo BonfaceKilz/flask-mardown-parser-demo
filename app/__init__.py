52987332ed2f7f2634e38d250753f46c68699307
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-10-25 13:52:45 +0300

    Use mistune to render markdown file
    * app/__init__.py: Add new "/test" path that parses the markdown
    files.
    * app/markdown/introduction.md: New file. The markdown file with free
    content.
    * app/templates/introduction.html: New file. HTML template where the
    markdown file is parsed.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 """Bootstrap the app"""
+import os
+
 from flask import Flask
 from flask import render_template
 
+import mistune
 
 app = Flask(__name__)
 app.config.from_object("config")
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Sample HTTP error handling
 @app.errorhandler(404)
@@ -19,3 +23,12 @@ def index():
     """Display homepage which is normal html"""
     return render_template("index.html"), 200
 
+
+@app.route("/test", methods=["get"])
+def parse_markdown_from_file():
+    """Parse markdown files from a file"""
+    with open(os.path.join(basedir,
+                           "markdown/introduction.md")) as md_file:
+        markdown = md_file.read()
+        return render_template("introduction.html",
+                               rendered_markdown=mistune.html(markdown)), 200
